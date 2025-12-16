efc8f0e679d26ef058c7022e80c5baf194c9e6a6
IMHICIHU <imhicihu@gmail.com>
2025-12-16 14:53:05 -0300

    End of life reached
    Signed-off-by: IMHICIHU <imhicihu@gmail.com>

--- code_convention.m
+++
@@ -1,8 +0,0 @@
-### Code convention
-* Add `//` comments to make code more human-readable
-* 4 spaces for indentation (no tabs)
-* 80 character line length *strongly* preferred
-* Prefer `'` over `"`
-* ES6 syntax when possible
-* Use semicolons; beyond [linter](https://en.wikipedia.org/wiki/Lint_(software))
-* Avoid abbr wrds
